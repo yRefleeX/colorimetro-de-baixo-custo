f1302c6f988da0a6a66bea8601af8c8c31123191
André Takeo Miiada Caseiro <andre.miiada@gmail.com>
2025-09-06 19:46:35 -0300

    Reversão do sistema de modelo 3D e otimização do app

--- Colorimetria/metro.config.js
+++
@@ -1,9 +0,0 @@
-// metro.config.js
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Adiciona suporte para a extensão de arquivo .stl
-config.resolver.assetExts.push('stl');
-
-module.exports = config;
