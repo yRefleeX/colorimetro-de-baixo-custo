d0b9ac47bea8e2b4cd8d8c570943b6d903c633e8
André Takeo Miiada Caseiro <andre.miiada@gmail.com>
2025-09-09 21:06:19 -0300

    Edição da configuração eslint.config.js

--- Colorimetria/eslint.config.js
+++ Colorimetria/eslint.config.js
@@ -1,7 +1,6 @@
   // https://docs.expo.dev/guides/using-eslint/
   const { defineConfig } = require('eslint/config');
   const expoConfig = require('eslint-config-expo/flat');
-  const importPlugin = require('eslint-plugin-import');
 
   module.exports = defineConfig([
     expoConfig,
