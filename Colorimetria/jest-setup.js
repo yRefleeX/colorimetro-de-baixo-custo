3ff05291e211fff82e110357cd862c4ae92d2fba
André Takeo Miiada Caseiro <andre.miiada@gmail.com>
2025-09-18 21:48:26 -0300

    Correção de jest-setup.js

--- Colorimetria/jest-setup.js
+++ Colorimetria/jest-setup.js
@@ -1,6 +1,6 @@
 // jest-setup.js
 require('@testing-library/jest-native/extend-expect');
-import 'dotenv/config';
+require('dotenv/config');
 
 jest.mock('@react-native-async-storage/async-storage', () =>
   require('@react-native-async-storage/async-storage/jest/async-storage-mock')
